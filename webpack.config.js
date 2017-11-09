b29dac886b3388d217fc0913ec2283c51a7aa967
xiexuanzheng123 <349328984@qq.com>
2017-11-09 18:20:23 +0800

    build the environment

---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+var webpack = require('webpack');
+//import webpack from 'webpack';
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+    devtool: 'cheap-module-polyfill',
+    entry: {
+        index: [
+            'babel-polyfill',
+            './src/index'
+        ]
+    },
+    output: {
+        path: path.join(__dirname, './build'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.less$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.(jpe?g|png|gif)$/i,
+                use: [
+                    'url-loader?limit=10000&name=img/[hash:8].[name].[ext]'
+                ]
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', '.json', '.css']
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new HtmlWebpackPlugin({
+            title: 'Three',
+            filename: 'build/d3.html',
+            template: './index.html',
+            inject: 'body',
+            hash: true,
+            chunks: ['index']
+            })
+        ]
+}
