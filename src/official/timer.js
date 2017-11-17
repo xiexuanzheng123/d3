dc13f6702357c748b7bada8ed434a0fbc9e634a9
xiexuanzheng123 <349328984@qq.com>
2017-11-17 15:38:12 +0800

    the usage of timer

---
+++ src/official/timer.js
@@ -0,0 +1,13 @@
+import * as d3 from 'd3';
+
+/**
+ * d3.now()
+ * 返回performance.now或Date.now
+ */
+console.log(d3.now());
+
+/**
+ * d3.timer(callback[, delay[, timer]])
+ * 定义新的timer定时器，一直 调用callback直到定时器被stopped
+ * 
+ */
